d826e5dcc1a7332fdec85422e6f7f655dc075078
sharoncabrera <ethepany.sharon@gmail.com>
2016-03-10 00:55:30 +0100

    Update op1.py

--- Challenge Scripting 2/op1.py
+++ Challenge Scripting 2/op1.py
@@ -1,6 +1,11 @@
 # OPERACIO 1: Numero de linies del document: NUM
 #OPERACIO 2: Numero de paraules del document:NUM
 #OPERACIO 3: Numero promig de paraules per linea del document:NUM
+#Generar un histograma, amb les vegades que tenim numero de paraules per linies del document,
+# indicant quantes linies tenen cada numero
+#Finalment haurem de triar una forma grafica de representar aquest histograma
+
+import pygal
 
 def contParaules(palabra):
 	words = palabra.split(" ")
@@ -11,7 +16,10 @@ def main():
 	contlinies=0
 	contParaula=0
 	string=""
-	#mmmm....NO SE SI VAN LAS EXCPTION, IGNASI TE ACUERDAS QUE NO BAJARON PUNTOS POR ESTO?O NO?
+	arraylinies=[]
+	arrayParaules=[]
+	arrayFreqParaules=[]
+	
 	try:
 		fitxer=open("textObama.txt")
 	except:
@@ -19,20 +27,27 @@ def main():
 	
 	fitxerOut=open("obama.OUT.txt","w")
 
-	for line in fitxer:
+	for i,line in enumerate(fitxer):
 		contlinies+=1
 		contParaula=contParaula + contParaules(line)
+		arrayParaules.append(contParaules(line))
+		arraylinies.append(i)
+		
+
 
 
 	fitxerOut.write("Numero de linies del document: %i \n" % contlinies)
 	fitxerOut.write("Numero de paraules del document %i \n" %contParaula)
 	fitxerOut.write("Numero promig de paraules per linea del document es: %i \n" % (contParaula/contlinies))
 
+	#Create a histogram
+	hist = pygal.Bar() # Then create a bar graph object
+	hist.add('Words', arrayParaules) # Add some values
+	
+	hist.render_to_file('Histograma.svg') # Save the svg to a file
+
 
 	fitxer.close()
 	fitxerOut.close()
 
 main()
-	
-
-
