b61dea562518f43c7446c1b2ef2eff03f667e3bd
ignasiargemipuig <ignasiargemi@gmail.com>
2016-03-10 18:54:43 +0100

    avancem

--- Challenge Scripting 2/op1.py
+++ Challenge Scripting 2/op1.py
@@ -7,47 +7,100 @@
 
 import pygal
 
-def contParaules(palabra):
-	words = palabra.split(" ")
-	return len(words)#devuelve numero elementos de la lista
+fitxer = open("textObama.txt", "r")
+fitxerOut = open("obama.OUT.txt","w")
+numlinies=0
+numparaules=0
+diccionari={}
+diccionarip={}
+longitud=0
 
-def main():
-	
-	contlinies=0
-	contParaula=0
-	string=""
-	arraylinies=[]
-	arrayParaules=[]
-	arrayFreqParaules=[]
-	
-	try:
-		fitxer=open("textObama.txt")
-	except:
-		print "El archivo no existe"
-	
-	fitxerOut=open("obama.OUT.txt","w")
+for linia in fitxer.readlines():
+        
+    if len(linia)>2:
+        paraules=linia.split()
+        for paraula in paraules:
+            paraula = paraula.replace('\n', '')
+            paraula = paraula.replace('\xe9', '')
+            paraula = paraula.replace('.','')
+            paraula = paraula.replace('', '')
+            paraula = paraula.replace('?', '')
+            paraula = paraula.replace('-', '')
+            paraula = paraula.replace(';', '')
+            paraula = paraula.replace(':', '')
+            paraula = paraula.replace('(', '')
+            paraula = paraula.replace(')', '')
+            paraula = paraula.replace("'", '')
+            paraula = paraula.replace(',', '')
+            paraula = paraula.replace('=', '')
+            paraula = paraula.replace('"', '')
+            paraula = paraula.replace('!', '')
+            paraula = paraula.replace(' ', '')
+            for lletra in paraula:
+            	lletra.lower()
+                if diccionari.has_key(lletra) and lletra!= ' ':
 
-	for i,line in enumerate(fitxer):
-		contlinies+=1
-		contParaula=contParaula + contParaules(line)
-		arrayParaules.append(contParaules(line))
-		arraylinies.append(i)
-		
+                    diccionari[lletra]=diccionari[lletra]+1
+
+                else:
+                    if lletra !=' ':
+                        diccionari.get(lletra)
+                        diccionari[lletra]=1
+
+            longitud= longitud + len(paraula)
+
+            if diccionarip.has_key(paraula):
+
+                diccionarip[paraula]=diccionarip[paraula]+ 1
+            else:
+                diccionarip.get(paraula)
+                diccionarip[paraula]=1
+
+        numlinies+=1
+        numparaules = numparaules + len(linia.split(None))
+
+##Transformacio de les dades
+llet = []
+for key, value in diccionari.iteritems():
+	print("Clau: " + str(key) + " Valor: " + str(value))
+	llet.append([key,value])
+
+print("\nParaules")
+par = []
+for key, value in diccionarip.iteritems():
+	print("Clau: " + str(key) + "\t\tValor: " + str(value))
+	par.append([key,value])
 
 
+mitpar = numparaules/numlinies
+mitlon = longitud/numparaules
 
-	fitxerOut.write("Numero de linies del document: %i \n" % contlinies)
-	fitxerOut.write("Numero de paraules del document %i \n" %contParaula)
-	fitxerOut.write("Numero promig de paraules per linea del document es: %i \n" % (contParaula/contlinies))
+fitxerOut.write("1. Numero de linies del document: "+str(numlinies)+ '\n\n')
 
-	#Create a histogram
-	hist = pygal.Bar() # Then create a bar graph object
-	hist.add('Words', arrayParaules) # Add some values
-	
-	hist.render_to_file('Histograma.svg') # Save the svg to a file
+fitxerOut.write("2. Numero de paraules del document: "+str(numparaules)+ '\n\n')
 
 
-	fitxer.close()
-	fitxerOut.close()
+fitxerOut.write("3. Mitjana de paraules per linia: "+str(mitpar)+ '\n\n')
+#Create a histogram
+hist = pygal.Bar(title='Paraules') # Then create a bar graph object
+for val in par:
+	k = val[0]
+	v = val[1]
+	hist.add(k, v) # Add some values
 
-main()
+hist.render_in_browser()
+#hist.render_to_file('Histograma1.svg')  # Save the svg to a file
+
+fitxerOut.write("4. Promig de lletres en el document:\n")
+fitxerOut.write(str(diccionari.keys()))
+fitxerOut.write("\n" + str(diccionari.values()) + "\n")
+
+fitxerOut.write("\n5. Promig de longitud de paraules en el document: ")
+fitxerOut.write(str(mitlon)+ "\n")
+
+fitxerOut.write("\n6. Promig de paraules en el document:\n")
+fitxerOut.write(str(diccionarip.keys()))
+fitxerOut.write("\n" + str(diccionarip.values()))
+
+fitxer.close()
+fitxerOut.close()
