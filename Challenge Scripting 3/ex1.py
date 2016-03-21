0dd1abd78beb24aebcd12a2c5a6ab55ce6347c7a
ignasiargemipuig <ignasiargemi@gmail.com>
2016-03-21 01:22:49 +0100

    FINAL

--- Challenge Scripting 3/ex1.py
+++ Challenge Scripting 3/ex1.py
@@ -1,9 +1,9 @@
 import pygal
 
-
-fitxer = open("LAST.log","r")
+nom = raw_input("Digam el nom del fitxer d'entrada:\n(Sense Extencio)\n")
+fitxer = open(nom + ".log", "r")
 opcio = raw_input("Digues quina opcio vols executar:\n(-l,-m VULNERABILITAT,-t VULNERABILITAT)\n")
-if opcio != "-l" and opcio != "-m" and opcio != "-t VULNERABILITAT":
+if opcio != "-l" and opcio != "-m VULNERABILITAT" and opcio != "-t VULNERABILITAT":
     print "Error: opcio no valida.\n"
 else:
     llegirTipus = False
@@ -52,14 +52,28 @@ else:
             print(str(key))
 
     elif opcio == "-t VULNERABILITAT":
-        print("Taula de vulnerabilitats:")
+        fitxerOUT = open("taula.html", "w")
+        fitxerOUT.write("<!DOCTYPE html>\n")
+        fitxerOUT.write("<html>\n")
+        fitxerOUT.write("  <head>\n")
+        fitxerOUT.write("Taula de vulnerabilitats\n")
+        fitxerOUT.write("  </head>\n")
+        fitxerOUT.write("  <body>\n")      
+        fitxerOUT.write("<table width=""100%"" border=""2"" cellpadding=""1"" cellspacing=""1"" bordercolor=""#000000"" style=""border-collapse:collapse;"">\n")
+        fitxerOUT.write("<tr>")
+        fitxerOUT.write("<th>Pais</th><th>Atacs</th>")        
+        fitxerOUT.write("</tr>\n")
         for key, value in vulnerabilitats.iteritems():
-            print("Tipus: " + str(key) + "\tNumero d'atacs: " + str(value))
+            fitxerOUT.write("<tr>")
+            fitxerOUT.write("<td>"+ key +"</td><td> "+ str(value) +"</td>")        
+            fitxerOUT.write("</tr>\n")
+        fitxerOUT.write("</table>\n")
+        fitxerOUT.write("  </body>\n")
+        fitxerOUT.close()
 
     else:
         worldmap_chart = pygal.maps.world.World()
         worldmap_chart.title = 'Mapamundi dels atacs'
-        
         worldmap_chart.add("Paisos", paisos)
-
         worldmap_chart.render_in_browser()
+fitxer.close()
