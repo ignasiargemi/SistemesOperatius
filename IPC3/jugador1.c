0ae0a4a919a3f9137c9cf32dd46ca748fdbff403
sharoncabrera <ethepany.sharon@gmail.com>
2016-03-23 01:18:30 +0100

    Added files via upload

--- IPC3/jugador1.c
+++ IPC3/jugador1.c
@@ -1,80 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <sys/stat.h>
 #include <fcntl.h>
-
 #include <sys/ipc.h>
-#include <sys/sem.h>
-#include <semaphore.h>
 
 
-void err_sys(const char* cadena)
-{
-	perror(cadena);
-	exit(1);
-} 
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#define BUFFSIZE 20
+
+void err_sys(const char* cadena){ perror(cadena);exit(1);} 
 
 
-int main()
-{
-	int 	jug1;
-	sem_t* 	JA1;
-	sem_t*	AJ1;	
-	int 	jugador1, jugador2;
-	int     tirada, suma;
+int main(int argc, char *argv[]) {
+
+    int sock;
+    struct sockaddr_in echoArbitre;
+    char buffer[BUFFSIZE];
+    unsigned int echolen;
+    int bytes = 0;
+
+      
+    int     jugador1, jugador2;
+    int     tirada, suma;
+
+    if (argc != 3) {
+    fprintf(stderr, "US: TCPecho <ip_servidor> <port>\n");
+    exit(1);}
+    /*Creamos socket*/
+    if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+    err_sys("errpr socket");}
 
 
-	if ((JA1 = (sem_t*)sem_open("/JA1", 0)) == SEM_FAILED) {
-    	err_sys("semaphore initilization");
-  	}
-  	if ((AJ1 = (sem_t*)sem_open("/AJ1", 0)) == SEM_FAILED) {
-    	err_sys("semaphore initilization");
-  	}
+    /* preparamos sockaddr_in */
+    memset(&echoArbitre, 0, sizeof(echoArbitre));       /* borra memoria */
+    echoArbitre.sin_family = AF_INET;                  /* Internet/IP */
+    echoArbitre.sin_addr.s_addr = inet_addr(argv[1]);  /* IP address */
+    echoArbitre.sin_port = htons(atoi(argv[2]));       /* server port */
 
-  	if ((jug1=open("./jugador1.txt",O_WRONLY|O_CREAT|O_TRUNC,0700)) < 0)
-		err_sys("error de fitxer sortida");
+    /* establecemos conexion con Arbitro */
+    if (connect(sock,(struct sockaddr *) &echoArbitre,sizeof(echoArbitre)) < 0) {
+                  err_sys("error connect");
+    }
 
-	sem_post(JA1);
+    
 
-	close(jug1);
+    while(1){
+        
+        
+        if ((read(sock, &jugador1, sizeof(jugador1))) < 0) {
+        err_sys("error recepcion");}
 
+        if ((read(sock, &jugador2, sizeof(jugador2))) < 0) {
+        err_sys("error recepcion");}
+        
+        
+        printf("Marcador: %i - ",jugador1);
+        printf("%i\n",jugador2);
+        
+        if (jugador1 >= 3 || jugador2 >= 3) break; //Final
+        
 
-	while(1){
-		sem_wait(AJ1);
-		jug1 = open("./jugador1.txt",O_RDONLY, 0);
-		read(jug1, &jugador1, sizeof(jugador1));
-		read(jug1, &jugador2, sizeof(jugador2));
-		printf("Marcador: %i - ",jugador1);
-		printf("%i\n",jugador2);
-		close(jug1);
-		if (jugador1 >= 3 || jugador2 >= 3) break; //Final
-		jug1 = open("./jugador1.txt",O_WRONLY, 0700);
-		do{
-			printf("Escriu el valor de la teva tirada:\n(0-3)\n");
-			scanf("%i",&tirada);
+        do{
+            printf("Escriu el valor de la teva tirada:\n(0-3)\n");
+            scanf("%i",&tirada);
 
-		}while(tirada>3 || tirada < 0);
+        }while(tirada>3 || tirada < 0);
 
-		write(jug1,&tirada,sizeof(tirada));
+        
+        if (write(sock, &tirada, sizeof(int)) != sizeof(int)) {
+            err_sys("error al enviar la informacio a servidor ");
+        }
 
-		do {
-			printf("Quina es la teva suma/aposta:\n(0-6)\n");
-			scanf("%i",&suma);
-		}while (suma < 0 || suma > 6);
-		
-		write(jug1,&suma,sizeof(suma));
+        do {
+            printf("Quina es la teva suma/aposta:\n(0-6)\n");
+            scanf("%i",&suma);
+        }while (suma < 0 || suma > 6);
 
-		printf("Tirada: %i - Suma: %i\n", tirada, suma);
-		
-		close(jug1);
+        if (write(sock, &suma, sizeof(int)) != sizeof(int)) {
+            err_sys("error al enviar la informacio a servidor ");
+        }
+ 
+        printf("Tirada: %i - Suma: %i\n", tirada, suma);
+        
+        
 
-		sem_post(JA1);
-	}
+        if (connect(sock,(struct sockaddr *) &echoArbitre,sizeof(echoArbitre)) < 0) {
+            err_sys("error connect");}
+    }
 
-	printf("Final Jugador 1!\n");
-	if (sem_close(AJ1)!=0) err_sys("error eliminació semàfor");
-	if (sem_close(JA1)!=0) err_sys("error eliminació semàfor");
-	
-	exit(0);
+    printf("Final Jugador 1!\n");
+    close(sock);
+    exit(0);
 }
