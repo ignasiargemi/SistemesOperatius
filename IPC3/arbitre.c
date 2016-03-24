49cf3687b820180ded3b3d73900f2b0b04b2b7cb
sharoncabrera <ethepany.sharon@gmail.com>
2016-03-24 13:49:32 +0100

    Added files via upload

--- IPC3/arbitre.c
+++ IPC3/arbitre.c
@@ -5,8 +5,6 @@
 #include <sys/ipc.h>
 
 #include <string.h>
-
-
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
@@ -29,10 +27,10 @@ if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0) {
 while (received > 0) {
   /* enviamos echo */
   if (send(sock, buffer, received, 0) != received) {
-      err_sys("error escriptura");
-  }
+      err_sys("error escriptura");}
+
     /* quedan datos por recibir */
-    if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0) {
+  if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0) {
       err_sys("error lectura addicional");
   }
 }
@@ -45,17 +43,18 @@ int main(int argc, char *argv[]) {
   int   jug1sock, jug2sock, arbitresock;
   struct sockaddr_in echoArbitre, echoJug1, echoJug2;
 
+
   int jugador1=0;
   int jugador2=0;
   int suma1, tirada1;
   int suma2, tirada2;
-  int r;
+  //int r;
 
   
   if (argc != 2) {err_sys("US: server <port>\n");}
 
   /* creamos socket TCP */
-  if ((arbitresock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+  if ((arbitresock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
   err_sys("error creacion socket");}
 
    /* preparamos sockaddr_in */
@@ -71,6 +70,8 @@ int main(int argc, char *argv[]) {
 
 
   printf("Inicialitzat\n");
+
+  // sem_wait(JA1);
 
   /* Listen --> El jugador1 ens avisa si ha entrat */
   if (listen(arbitresock, MAXPENDING) < 0) {
@@ -91,7 +92,7 @@ int main(int argc, char *argv[]) {
 
   printf("Jugador 1 a punt\n");
 
-  //Nos quedamos en wait
+  //sem_wait(JA2);
 
   /* Listen --> El jugador2 ens avisa si ha entrat */
   if (listen(arbitresock, MAXPENDING) < 0) {
@@ -122,9 +123,13 @@ int main(int argc, char *argv[]) {
   if (write(jug2sock, &jugador1, sizeof(int)) != sizeof(int)) {
             err_sys("error al enviar la informacio a servidor ");}
 
-
   if (write(jug2sock, &jugador2, sizeof(int)) != sizeof(int)) {
             err_sys("error al enviar la informacio a servidor ");}
+
+
+  //sem_post(AJ1);
+  //sem_post(AJ2);
+
 
 
 
@@ -134,17 +139,9 @@ int main(int argc, char *argv[]) {
   while (1) {
     printf("El marcador és de %i a %i\n",jugador1,jugador2);
     if (jugador1 >= 3 || jugador2 >= 3) break;
-    //w-->jug1
-    while (1) {
-       unsigned int jug1len = sizeof(echoJug1);
-      /* esperamso conexion por parte del cliente */
-      if ((jug1sock =
-        accept(arbitresock, (struct sockaddr *) &echoJug1,&jug1len)) < 0) {
-      err_sys("error accept");
-      }
-      fprintf(stdout, "Jugador: %s\n",inet_ntoa(echoJug1.sin_addr));
-      HandleJugador(jug1sock);
-    }
+
+    //sem_wait(JA1);
+    
 
     //Read
     if ((read(jug1sock, &tirada1, sizeof(tirada1))) < 0) {
@@ -153,17 +150,11 @@ int main(int argc, char *argv[]) {
     err_sys("error recepcion");}
     
     printf("Jugador 1: %i - %i\n", tirada1,suma1);
-    //w-->jug2
-    while (1) {
-      unsigned int jug2len = sizeof(echoJug2);
-      /* esperando conexion por parte del cliente */
-      if ((jug2sock =
-        accept(arbitresock, (struct sockaddr *) &echoJug2,&jug2len)) < 0) {
-        err_sys("error accept");
-      }
-      fprintf(stdout, "Jugador2: %s\n",inet_ntoa(echoJug2.sin_addr));
-      HandleJugador(jug2sock);
-  }
+
+
+    //sem_wait(JA2);
+    
+
 
 
     //Read
@@ -186,6 +177,8 @@ int main(int argc, char *argv[]) {
     if (write(jug2sock, &jugador2, sizeof(int)) != sizeof(int)) {
             err_sys("error al enviar la informacio a servidor ");}
     
+    //sem_post(AJ1);
+    //sem_post(AJ2);
 
   
   }
